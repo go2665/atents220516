36a2cabeccb12dceedbc6a044d8c1d8bb812eb37
go2665 <77224762+go2665@users.noreply.github.com>
2022-05-16 21:49:11 +0900

    변수 설명

--- 01_Console/01_Console/Program.cs
+++ 01_Console/01_Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _01_Console
 {
@@ -6,7 +6,16 @@ namespace _01_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");      // Console.WriteLine은 파라메터로 받은 문자열을 출력하는 함수
+            Console.WriteLine("헬로 월드 고병조");
+
+            // 변수 -> 변하는 숫자
+            // 변수의 종류 -> data type
+            // int, float, bool, string
+            // int 정수 -> 소수점이 없는 숫자(0, 1, 5, -7, 10)
+            // float 실수 -> 소수점이 있는 숫자( 0.0, 3.14, -55.9 )
+            // bool 불 -> true 아니면 false만 가지는 변수
+            // string 문자열 -> ""안에 있는 문장을 저장하는 변수( "개구리", "Hello World!" )
         }
     }
 }
