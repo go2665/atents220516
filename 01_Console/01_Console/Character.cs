486afa2f1736c69d6be35164d46b3b1549b0d15a
go2665 <77224762+go2665@users.noreply.github.com>
2022-05-19 19:45:11 +0900

    Human클래스가 Character클래스를 상속받음

--- 01_Console/01_Console/Character.cs
+++ 01_Console/01_Console/Character.cs
@@ -4,25 +4,15 @@ using System.Text;
 
 namespace _01_Console
 {
-    class Orc : Character
+    // 추상화 : 공통적 부분을 뽑아 일반화 하는 것
+    class Character
     {
-        // 데이터
-        // HP 있어야 함
-        // 그외 스텟은 원하는대로
-
-        // 기능
-        // 공격할 수 있다. Attack
-        // 피해를 입을 수 있다. TakeDamage
-        // 상태 출력 PrintStatus
-
-        // 데이터 -> 맴버 변수(필드)로 표현
-        private int healthPoint = 100;
-        private int healthPointMax = 100;
-        private int strength = 10;
-        private int dexterity = 10;
+        protected int healthPoint = 100;
+        protected int healthPointMax = 100;
+        protected int strength = 10;
+        protected int dexterity = 10;        
         public string name = "이름";
 
-        // 프로퍼티 : 특수한 함수(매서드)
         public int HealthPoint
         {
             get // HealthPoint를 읽을 때 실행되는 함수.
@@ -31,47 +21,36 @@ namespace _01_Console
             }
             set // HealthPoint 프로퍼티에 값을 넣을 때 실행되는 함수. 설정되는 값은 value라는 키워드에 들어있다.
             {
-                healthPoint = value;           
+                healthPoint = value;
                 healthPoint = Math.Clamp(healthPoint, 0, healthPointMax);
             }
         }
 
         public int Strength
         {
-            get
-            {
-                return strength;
-            }
+            get => strength;
         }
 
         public int Dexterity
         {
-            get => dexterity;   // 아래 주석과 같은 내용
+            get => dexterity; 
         }
 
-        // 생성자(Constructor) : 클래스이름과 같은 이름이어야 한다.
-        public Orc()
+        // 생성자(Constructor)
+        public Character()
         {
-            name = "새 오크";
-            strength = 30;
+            strength = 10;
             dexterity = 10;
+            name = "새 캐릭터";
         }
 
-        // 기능 -> 맴버 함수(매서드)로 표현
-        public void Attack(Human attackTarget)
+        // 부모 클래스로 된 변수는 자식 클래스도 담을 수 있다.
+        public void Attack(Character attackTarget)
         {
             // 공격할 수 있다.
-            Console.WriteLine($"{name}은(는) 인간 {attackTarget.name}를 공격합니다.");
+            Console.WriteLine($"{name}은(는) {attackTarget.name}를 공격합니다.");
             Console.WriteLine($"{name}은(는) {strength} 만큼 피해를 줍니다.");
-            attackTarget.TakeDamage((int)(strength * 1.2f));  // 20% 만큼 더 데미지를 주고 싶다. 다만 소수점 아래는 버려진다.
-        }
-
-        public void Attack(Orc attackTarget)
-        {
-            // 공격할 수 있다.
-            Console.WriteLine($"{name}은(는) 오크 {attackTarget.name}를 공격합니다.");
-            Console.WriteLine($"{name}은(는) {strength} 만큼 피해를 줍니다.");
-            attackTarget.TakeDamage((int)(strength * 1.2f));
+            attackTarget.TakeDamage(strength);
         }
 
         public void TakeDamage(int damage)
@@ -86,12 +65,12 @@ namespace _01_Console
             PrintStatus();
         }
 
-        public void PrintStatus()
+        public virtual void PrintStatus()
         {
             Console.WriteLine($"┌ {name,5} 스테이터스 ───────────────────┐");
             Console.WriteLine($"│   HP : {healthPoint,5}                           │");
             Console.WriteLine($"│   힘 : {strength,5}                           │");
-            Console.WriteLine($"│ 민첩 : {dexterity,5}                           │");            
+            Console.WriteLine($"│ 민첩 : {dexterity,5}                           │");
             Console.WriteLine($"└────────────────────────────────────────┘");
         }
     }
--- 01_Console/01_Console/Human.cs
+++ 01_Console/01_Console/Human.cs
@@ -5,77 +5,33 @@ using System.Text;
 // 접근제한자(Access Modifier, 액세스 한정자)
 // public  : (공용.) 누구든지 다 사용 가능. 다른 클래스와 데이터를 주고 받을 일이있을 때 사용. 최소한으로 만들어야 함.
 // private : (개인의, 사적인.) 나만 사용 가능
+// protected : 나랑 내 자식만 본다.
 
 
 namespace _01_Console
 {
     // 클래스 : 특정한 유형의 물체를 코드로 표현한 것(붕어빵틀)
-    class Human
+    // 상속 : 다른 클래스의 변수와 함수를 물려받는 것
+    class Human : Character
     {
         // 데이터 -> 맴버 변수(필드)로 표현
-        private int healthPoint = 100;
-        private int healthPointMax = 100;
-        private int strength = 10;
-        private int dexterity = 10;
         private int wisdom = 10;
-        public string name = "이름";
 
         // 프로퍼티 : 특수한 함수(매서드)
-        public int HealthPoint
-        {
-            get // HealthPoint를 읽을 때 실행되는 함수.
-            {
-                return healthPoint;
-            }
-            set // HealthPoint 프로퍼티에 값을 넣을 때 실행되는 함수. 설정되는 값은 value라는 키워드에 들어있다.
-            {
-                //strength = value;
-                healthPoint = value;
-
-
-                //if(healthPoint > healthPointMax)
-                //{
-                //    healthPoint = healthPointMax;
-                //}
-                //else if( healthPoint < 0 )
-                //{
-                //    healthPoint = 0;
-                //}                
-                healthPoint = Math.Clamp(healthPoint, 0, healthPointMax);
-            }
-        }
-
-        public int Strength
-        {
-            get
-            {
-                return strength;
-            }
-        }
-
-        public int Dexterity
-        {
-            get => dexterity;   // 아래 주석과 같은 내용
-            //get
-            //{
-            //    return dexterity;
-            //}
-        }
-
         public int Wisdom
         {
             get => wisdom;
         }
 
         // 생성자(Constructor)
-        public Human()
-        {
+        public Human() : base()
+        {            
             strength = 10;
             dexterity = 10;
             wisdom = 10;
         }
 
-        public Human(int str, int dex, int wis, string newName = "새이름")
+        public Human(int str, int dex, int wis, string newName = "새휴먼")
         {
             strength = str;
             dexterity = dex;
@@ -84,36 +40,9 @@ namespace _01_Console
         }        
 
         // 기능 -> 맴버 함수(매서드)로 표현
-        public void Attack(Human attackTarget)
+        public override void PrintStatus()
         {
-            // 공격할 수 있다.
-            Console.WriteLine($"{name}은(는) {attackTarget.name}를 공격합니다.");
-            Console.WriteLine($"{name}은(는) {strength} 만큼 피해를 줍니다.");
-            attackTarget.TakeDamage(strength);
-        }
-
-        public void Attack(Orc attackTarget)
-        {
-            // 공격할 수 있다.
-            Console.WriteLine($"{name}은(는) {attackTarget.name}를 공격합니다.");
-            Console.WriteLine($"{name}은(는) {strength} 만큼 피해를 줍니다.");
-            attackTarget.TakeDamage(strength);
-        }
-
-        public void TakeDamage(int damage)
-        {
-            // 데미지를 받을 수 있다.
-            Console.WriteLine($"{name}은(는) {damage} 만큼 피해를 입었습니다.");            
-            HealthPoint -= damage;
-            if ( HealthPoint <= 0 )     // 비교를 할때는 최대한 큰 범위로 비교를 해야한다.(나중에 실수나 수정할 일이 대체로 줄어든다)
-            {
-                Console.WriteLine($"{name}은(는) 죽었다.");
-            }
-            PrintStatus();
-        }
-
-        public void PrintStatus()
-        {
+            // base.PrintStatus();
             Console.WriteLine($"┌ {name,5} 스테이터스 ───────────────────┐");
             Console.WriteLine($"│   HP : {healthPoint,5}                           │");
             Console.WriteLine($"│   힘 : {strength,5}                           │");
