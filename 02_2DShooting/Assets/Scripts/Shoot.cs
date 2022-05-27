4c8712d0b87f8812bea2f88bf3d356da367f5227
go2665 <77224762+go2665@users.noreply.github.com>
2022-05-27 19:57:02 +0900

    2D 슈팅 - 총알에 수명 설정하기

--- 02_2DShooting/Assets/Scripts/Shoot.cs
+++ 02_2DShooting/Assets/Scripts/Shoot.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))] 
 public class Shoot : MonoBehaviour
 {
+    public float lifeTime = 3.0f;   // 총알의 수명
     public float speed = 10.0f;
     Rigidbody2D rigid = null;
 
@@ -18,5 +19,19 @@ public class Shoot : MonoBehaviour
     {
         // 백터 : 힘의 방향과 크기
         rigid.velocity = transform.right * speed;
+
+        Destroy(this.gameObject, lifeTime);     // lifeTime초 후에 게임 오브젝트를 삭제한다.
     }
+
+    // Update 함수를 이용해 일정 시간 후에 삭제하기
+    //private void Update()
+    //{
+    //    // Time.deltaTime;  // 이전 업데이트 함수가 호출되고 현제 업데이트 함수가 호출될 때까지의 시간                
+    //    lifeTime -= Time.deltaTime;
+    //    if( lifeTime < 0.0f )
+    //    {
+    //        //죽으면 된다.
+    //        Destroy(this.gameObject);
+    //    }
+    //}
 }
