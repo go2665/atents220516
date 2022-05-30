a52e76937bfc1cb284aa981996c107136015ba52
go2665 <77224762+go2665@users.noreply.github.com>
2022-05-30 19:49:11 +0900

    2D 슈팅 - 적 쏴서 맞추기

--- 02_2DShooting/Assets/Scripts/Shoot.cs
+++ 02_2DShooting/Assets/Scripts/Shoot.cs
@@ -34,4 +34,28 @@ public class Shoot : MonoBehaviour
     //        Destroy(this.gameObject);
     //    }
     //}
+
+    /// <summary>
+    /// 서로 충돌했을 때 실행되는 함수(이 스크립트를 가지고 있는 게임 오브젝트의 컬라이더에 다른 컬라이더가 충돌했을 때 실행)
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log($"OnCollisionEnter2D : {collision.gameObject.name}");
+    }
+
+    /// <summary>
+    /// 서로 겹쳤을 때 실행되는 함수(이 스크립트를 가지고 있는 게임 오브젝트의 컬라이더가 다른 트리거에 들어갔을 때 실행)
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log($"OnTriggerEnter2D : {collision.gameObject.name}");
+        //if (collision.tag == "KillZone") ;    // 매우 좋지 않음
+        if( collision.CompareTag("KillZone") )  // 해시 (Hash) -> 유니크한 요약본을 만들어준다.
+        {
+            // 킬존에 들어갔다.
+            Destroy(this.gameObject);
+        }
+    }
 }
