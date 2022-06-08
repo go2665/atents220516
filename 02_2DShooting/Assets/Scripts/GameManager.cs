c18037dd6e8be30327043240d6ca416ab8cd0a8a
go2665 <77224762+go2665@users.noreply.github.com>
2022-06-08 19:24:43 +0900

    2D 슈팅 - 게임메니저 작성

---
+++ 02_2DShooting/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int score = 0;      // 점수 저장용
+    private static GameManager instance = null; // static이 붙어서 주소가 고정이다.
+
+    // 프로퍼티(속성). 특이한 메서드(함수). 읽기 전용, 쓰기 전용 등으로 설정해서 객체지향적 특성을 유지할 수 있음.
+    // 값을 쓸거나 읽을 때 실행되어야 할 기능들을 쉽게 추가할 수 있다.
+    public static GameManager Inst 
+    {
+        //get
+        //{
+        //    return instance;
+        //}
+        get => instance;
+    }
+
+    public int Score
+    {
+        get => Score;
+        set
+        {
+            score = value;
+            Debug.Log($"Score : {score}");
+        }
+    }
+
+    private void Awake()
+    {
+        // 디자인 패턴 : ~식으로 코딩을 하니 좋더라 하는 것들
+        // 싱글톤(Singleton) : 디자인 패턴 중 하나. 클래스의 인스턴스가 단 하나만 존재하도록 만드는 것
+        if( instance == null )
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject); // 다른 씬이 로드되어도 게임 오브젝트 유지
+        }
+        else
+        {
+            // 먼저 만들어진 GameManager가 있다.
+            if(instance != this)    // 먼저 만들어진 것이 내가 아닐 때
+            {
+                Destroy(this.gameObject);   // 자기 자신이 제거된다.
+            }
+        }
+    }
+}
--- 02_2DShooting/Assets/Scripts/Test2.cs
+++ 02_2DShooting/Assets/Scripts/Test2.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class Test2 : MonoBehaviour
 {
+    int i = 10;
+    static int j = 20;
+
     private void Awake()
     {
         // 이 게임 오브젝트의 자식 오브젝트 중에서 같은 종류의 컴포넌트를 찾음
         SpriteRenderer renderer = GetComponentInChildren<SpriteRenderer>();
         renderer.flipX = true;  // flipX 설정(체크함)
         renderer.flipY = false; // flipY 해제(체크해제됨)
+    }
+
+    public void TestTest()
+    {
+        j = 30;
+
+        Test2.TestTest2();  // static 함수는 객체를 생성하지 않아도 사용할 수 있다.
+    }
+
+    public static void TestTest2()
+    {
+        //i = 20;   //안됨. static은 static만 접근 가능
+        j = 30;
+
+        //GameManager.Inst.Test();
     }
 }
