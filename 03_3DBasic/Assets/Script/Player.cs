85b38479c5aa7fd47370e6dede9b1769ceff769d
go2665 <77224762+go2665@users.noreply.github.com>
2022-06-13 18:13:51 +0900

    3D 기본 - 입력 처리 완료

--- 03_3DBasic/Assets/Script/Player.cs
+++ 03_3DBasic/Assets/Script/Player.cs
@@ -1,14 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
+    public float turnSpeed = 180.0f;
     PlayerInputActions actions = null;
     Vector3 inputDir = Vector3.zero;
+    float inputSide = 0.0f;
 
     Rigidbody rigid = null;
 
@@ -39,7 +38,11 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         // inputDir의 y값을 이용하여 이 오브젝트의 앞쪽 방향(transform.forward)으로 이동
-        rigid.MovePosition(rigid.position + transform.forward * inputDir.y * moveSpeed * Time.fixedDeltaTime);
+        // inputSide를 이용해서 이 오브젝트의 오른쪽 방향(transform.right)으로 이동
+        rigid.MovePosition(rigid.position 
+            + moveSpeed * Time.fixedDeltaTime * (inputDir.y * transform.forward + inputSide * transform.right));
+
+        rigid.MoveRotation(rigid.rotation * Quaternion.AngleAxis(inputDir.x * turnSpeed * Time.fixedDeltaTime, transform.up));
     }
 
     private void OnMoveInput(InputAction.CallbackContext context)
@@ -50,6 +53,7 @@ public class Player : MonoBehaviour
 
     private void OnSideMoveInput(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<float>());
+        //Debug.Log(context.ReadValue<float>());
+        inputSide = context.ReadValue<float>();
     }
 }
