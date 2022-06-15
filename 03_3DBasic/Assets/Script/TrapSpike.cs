b6145c8bcebfbc9496cf8d394143e641b58c0562
go2665 <77224762+go2665@users.noreply.github.com>
2022-06-15 17:54:25 +0900

    3D 기본 - 함정만들기 완료

---
+++ 03_3DBasic/Assets/Script/TrapSpike.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSpike : MonoBehaviour
+{
+    Animator anim = null;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if( other.CompareTag("Player"))
+        {
+            anim.SetTrigger("Activate");
+        }
+    }
+}
