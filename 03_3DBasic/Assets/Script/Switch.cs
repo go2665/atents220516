ef7daf74881bab5c9ee223e0226221e2bf901081
go2665 <77224762+go2665@users.noreply.github.com>
2022-06-17 19:44:42 +0900

    3D 기본 - 스위치 문 완료

---
+++ 03_3DBasic/Assets/Script/Switch.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour, IUseable
+{
+    public Door door = null;
+
+    Animator anim = null;
+    bool switchOnOff = false;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+        // 자식 중 이름이 "Door_Switch"인 게임 오브젝트를 찾아 Door 컴포넌트 가져오기
+        door = transform.Find("Door_Switch2")?.GetComponent<Door>();  
+    }
+
+    public void Use()
+    {
+        switchOnOff = !switchOnOff; // not(불변수 뒤집기)
+        anim.SetBool("IsSwitchOn", switchOnOff);
+
+        if( switchOnOff == true )
+        {
+            door?.Open();
+        }
+        else
+        {
+            door?.Close();
+        }
+    }
+}
