12eb68e67afa19d67d6dcff48b759d009957df6d
go2665 <77224762+go2665@users.noreply.github.com>
2022-06-17 20:18:05 +0900

    Update Switch.cs

--- 03_3DBasic/Assets/Script/Switch.cs
+++ 03_3DBasic/Assets/Script/Switch.cs
@@ -14,7 +14,7 @@ public class Switch : MonoBehaviour, IUseable
         anim = GetComponent<Animator>();
 
         // 내 부모의 자식 중 이름이 "Door_Switch"인 게임 오브젝트를 찾아 Door 컴포넌트 가져오기
-        door = transform.parent.transform.Find("Door_Switch")?.GetComponent<Door>();  
+        door = transform.parent.Find("Door_Switch")?.GetComponent<Door>();  
     }
 
     public void Use()
