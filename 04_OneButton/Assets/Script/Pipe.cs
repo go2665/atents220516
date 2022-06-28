67cc4244c41b761cf85f3e6f84e06d3ed775ad83
go2665 <77224762+go2665@users.noreply.github.com>
2022-06-28 18:03:18 +0900

    2D 원버튼 - 파이프 움직이기

---
+++ 04_OneButton/Assets/Script/Pipe.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    /// <summary>
+    /// 파이프의 최소 높이
+    /// </summary>
+    public float min = 0.0f;
+
+    /// <summary>
+    /// 파이프의 최대 높이
+    /// </summary>
+    public float max = 4.0f;
+
+    /// <summary>
+    /// 리지드바디
+    /// </summary>
+    Rigidbody2D rigid = null;
+
+    /// <summary>
+    /// 랜덤으로 정해진 높이
+    /// </summary>
+    float randomHeight = 0.0f;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {        
+        ResetHeight();  // 시작할 때 높이 결정
+    }
+
+    /// <summary>
+    /// 파이프의 높이를 랜덤으로 리셋하는 함수
+    /// </summary>
+    public void ResetHeight()
+    {
+        randomHeight = Random.Range(min, max);  //랜덤으로 높이 구하기
+    }
+
+    /// <summary>
+    /// 파이프를 이동시키는 함수
+    /// </summary>
+    /// <param name="moveDelta">파이프 이동 벡터(방향과 크기)</param>
+    public void Move(Vector2 moveDelta)
+    {
+        // 한 프레임안에 MovePosition을 두 번 호출하면 마지막 것만 적용이 됨
+
+        Vector2 pos = new(rigid.position.x, randomHeight);  // 높이를 MovePosition으로 움직이려고 하니 문제가 많았음.
+                                                            // 새 현재 위치 만들어서 이걸 기준으로 움직임.        
+                                                            // 높이는 항상 randomHeight에 설정된 높이로.
+        rigid.MovePosition(pos + moveDelta);    // MovePosition으로 이동시키기
+    }
+}
---
+++ 04_OneButton/Assets/Script/PipeRotator.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 파이프들을 계속 재활용하며 움직이는 클래스
+/// </summary>
+public class PipeRotator : MonoBehaviour
+{
+    /// <summary>
+    /// 파이프들의 이동 속도
+    /// </summary>
+    public float moveSpeed = 6.0f;
+
+    /// <summary>
+    /// 파이프간의 간격
+    /// </summary>
+    public float width = 4.0f;
+
+    /// <summary>
+    /// 파이프가 밖으로 나간 것을 확인할 x좌표 값
+    /// </summary>
+    const float X_RANGE_OUT = -10.0f;
+
+    /// <summary>
+    /// PipeRotator가 가지고 있는 파이프들
+    /// </summary>
+    Pipe[] children;
+
+    private void Start()
+    {
+        children = new Pipe[transform.childCount];          // 자식 파이프 수만큼 배열 확보
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children[i] = transform.GetChild(i).GetComponent<Pipe>();   // 파이프 가져와서 저장하고
+            children[i].Move(width * i * Vector2.right);    // 초기 위치 설정
+            children[i].ResetHeight();                      // 높이도 초기화
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (Pipe pipe in children)
+        {
+            pipe.Move(Time.fixedDeltaTime * moveSpeed * Vector2.left);      // 우선 파이프를 왼쪽으로 움직임
+
+            if( pipe.transform.position.x < X_RANGE_OUT)                    // 파이프가 X축 기준으로 일정 이상 이동했으면
+            {
+                pipe.Move(width * transform.childCount * Vector2.right);    // 오른쪽 끝으로 이동시킴
+                pipe.ResetHeight();                                         // 높이도 다시 랜덤으로 리셋
+            }
+        }
+    }
+}
