8b20a91a913e3468251c9ff64e5b9bd4aa187ef3
go2665 <77224762+go2665@users.noreply.github.com>
2022-06-29 20:45:14 +0900

    2D 원버튼 - 파일 저장 및 읽기(주석 추가)

--- 04_OneButton/Assets/Script/GameManager.cs
+++ 04_OneButton/Assets/Script/GameManager.cs
@@ -75,33 +75,33 @@ public class GameManager : MonoBehaviour
 
     public void SaveGameData()
     {
-        SaveData saveData = new();
-        saveData.highScore = highScore;
+        SaveData saveData = new();          // json 저장용 클래스 인스턴스화
+        saveData.highScore = highScore;     // json 저장용 클래스에 값을 넣기
 
-        string json = JsonUtility.ToJson(saveData);
+        string json = JsonUtility.ToJson(saveData);     // json 저장용 클래스의 내용을 json포맷의 문자열로 변경
 
-        string path = $"{Application.dataPath}/Save/";
-        if( !Directory.Exists(path) )
+        string path = $"{Application.dataPath}/Save/";  // 저장할 폴더의 경로 구하기
+        if( !Directory.Exists(path) )           // 경로가 존재하는지 확인
         {
             // path 폴더가 없다.
-            Directory.CreateDirectory(path);
+            Directory.CreateDirectory(path);    // 경로에 해당하는 폴더가 없으면 만들기
         }
 
-        string fullPath = $"{path}Save.json";
-        File.WriteAllText(fullPath, json);
+        string fullPath = $"{path}Save.json";   // 경로명과 파일명을 합쳐서 전체경로(fullPath) 만들기
+        File.WriteAllText(fullPath, json);      // 전체경로대로 파일을 만들어서 json 문자열 내용을 쓰기
     }
 
     public void LoadGameData()
     {
-        string path = $"{Application.dataPath}/Save/";
-        string fullPath = $"{path}Save.json";
+        string path = $"{Application.dataPath}/Save/";          // 저장된 폴더 이름 만들기
+        string fullPath = $"{path}Save.json";                   // 전체경로 만들기
 
-        if( Directory.Exists(path) && File.Exists(fullPath) )
+        if( Directory.Exists(path) && File.Exists(fullPath) )   // 경로와 파일 둘 다 존재하는지 확인
         {
-            string json = File.ReadAllText(fullPath);
-            SaveData saveData = JsonUtility.FromJson<SaveData>(json);
-            highScore = saveData.highScore;
-            Debug.Log($"High Score : {saveData.highScore}");
+            string json = File.ReadAllText(fullPath);           // 실제로 파일에 써있는 문자열 읽기
+            SaveData saveData = JsonUtility.FromJson<SaveData>(json);   // 특정 클래스(SaveData) 규격에 맞게 파싱하기
+            highScore = saveData.highScore;                     // json 데이터를 불러온 클래스에서 원하는 값 가져오기
+            //Debug.Log($"High Score : {saveData.highScore}");
         }
     }
 }
