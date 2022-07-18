af237b4a229165dbc92fd1817f4f9307e6f5ed05
go2665 <77224762+go2665@users.noreply.github.com>
2022-07-18 19:30:43 +0900

    Action - 미니맵에 아이템용 인디케이터 추가

--- 05_Action/Assets/Scripts/Item/ItemFactory.cs
+++ 05_Action/Assets/Scripts/Item/ItemFactory.cs
@@ -27,6 +27,10 @@ public class ItemFactory
         col.radius = 0.5f;
         col.isTrigger = true;
         itemCount++;    // 생성할 때마다 값을 증가시켜서 중복이 없도록 처리
+
+        GameObject indicator = Resources.Load<GameObject>("MinimapItemIndicator_Item");
+        GameObject.Instantiate(indicator, 
+            obj.transform.position, obj.transform.rotation * Quaternion.Euler(90, 0, 0), obj.transform);
         
 
         return obj;     // 생성완료된 것 리턴
