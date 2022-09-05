b6fbc14f8040ebdaf283039612109f4c379b4079
go2665 <77224762+go2665@users.noreply.github.com>
2022-09-05 19:17:58 +0900

    Tank - 포탄 주석 처리

--- 07_QuaterView/Assets/Scripts/Shell.cs
+++ 07_QuaterView/Assets/Scripts/Shell.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Shell : MonoBehaviour
 {
-    public float initialSpeed = 5.0f;
-    public GameObject explosionPrefab;
+    public float initialSpeed = 5.0f;       // 생성되면 즉시 적용될 속도
+    public GameObject explosionPrefab;      // 폭팔 이팩트 프리팝
 
     Rigidbody rigid;
 
@@ -17,15 +17,16 @@ public class Shell : MonoBehaviour
 
     private void Start()
     {
-        rigid.velocity = transform.forward * initialSpeed;
+        rigid.velocity = transform.forward * initialSpeed;  // 시작하면 앞쪽 방향으로 initialSpeed만큼의 속도로 나간다.
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        GameObject obj = Instantiate(explosionPrefab, 
-            collision.contacts[0].point,                            // 충돌지점
-            Quaternion.LookRotation(collision.contacts[0].normal)); // 충돌지점의 노멀백터를 forward로 지정
+        // 충돌하면 폭팔 이팩트 생성
+        Instantiate(explosionPrefab, 
+            collision.contacts[0].point,                            // 생성 위치는 충돌지점
+            Quaternion.LookRotation(collision.contacts[0].normal)); // 생성될 때의 회전은 충돌지점의 노멀백터를 forward로 지정하는 회전
 
-        Destroy(this.gameObject);
+        Destroy(this.gameObject);   // 포탄 삭제
     }
 }
--- 07_QuaterView/Assets/Scripts/Tank.cs
+++ 07_QuaterView/Assets/Scripts/Tank.cs
@@ -7,32 +7,33 @@ using UnityEngine.InputSystem;
 
 public class Tank : MonoBehaviour
 {
-    public GameObject shellPrefab;
-    public Transform firePosition;
+    public GameObject shellPrefab;      // 포탄용 프리팹
+    public Transform firePosition;      // 발사 위치
 
-    public float moveSpeed = 3.0f;
-    public float turnSpeed = 3.0f;
-    public float turretTurnSpeed = 0.05f;
+    public float moveSpeed = 3.0f;          // 이동 속도
+    public float turnSpeed = 3.0f;          // 회전 속도
+    public float turretTurnSpeed = 0.05f;   // 포탑 회전 속도
 
-    private Transform turret;
-    private Quaternion turretTargetRotation = Quaternion.identity;
+    private Transform turret;               // 포탑의 트랜스폼
+    private Quaternion turretTargetRotation = Quaternion.identity;  // 포탑의 회전
 
-    TankInputActions inputActions;
+    TankInputActions inputActions;          // 액션맵
 
-    Vector2 inputDir = Vector2.zero;
+    Vector2 inputDir = Vector2.zero;        // 이력받은 이동 방향
 
     Rigidbody rigid;
 
     private void Awake()
     {
-        inputActions = new TankInputActions();
-        rigid = GetComponent<Rigidbody>();
-        turret = transform.Find("TankRenderers").Find("TankTurret");
-        firePosition = turret.GetChild(0);
+        inputActions = new TankInputActions();  // 액션맵 생성
+        rigid = GetComponent<Rigidbody>();      // 리지드바디 가져오기
+        turret = transform.Find("TankRenderers").Find("TankTurret");    // 포탑 찾고
+        firePosition = turret.GetChild(0);      // 발사 위치 찾기
     }
 
     private void OnEnable()
     {
+        // 입력 액션과 함수 연결
         inputActions.Tank.Enable();
         inputActions.Tank.Move.performed += OnMove;
         inputActions.Tank.Move.canceled += OnMove;
@@ -42,6 +43,7 @@ public class Tank : MonoBehaviour
 
     private void OnDisable()
     {
+        // 입력 액션과 함수 연결 해제
         inputActions.Tank.NormalFire.performed -= OnNormalFire;
         inputActions.Tank.Look.performed -= OnMouseMove;
         inputActions.Tank.Move.canceled -= OnMove;
@@ -51,41 +53,44 @@ public class Tank : MonoBehaviour
 
     void OnMove(InputAction.CallbackContext context)
     {
-        inputDir = context.ReadValue<Vector2>();
+        inputDir = context.ReadValue<Vector2>();    // 이동 입력 받은 것 전달.
     }
 
     void OnMouseMove(InputAction.CallbackContext context)
     {
-        Vector2 screenPos = context.ReadValue<Vector2>();
-        Ray ray = Camera.main.ScreenPointToRay(screenPos);
-        if( Physics.Raycast(ray, out RaycastHit hit, 1000.0f, LayerMask.GetMask("Ground")) )
+        // 마우스가 움직일 때만 실행
+        Vector2 screenPos = context.ReadValue<Vector2>();       // 마우스의 위치 = 마우스의 스크린좌표
+        Ray ray = Camera.main.ScreenPointToRay(screenPos);      // 마우스의 스크린좌표를 이용해 레이 계산
+        if( Physics.Raycast(ray, out RaycastHit hit, 1000.0f, LayerMask.GetMask("Ground")) )    // 레이를 이용해 레이케스팅
         {
-            //Debug.Log("피킹");
             //hit.point;    // 마우스가 가리키는 땅 위치
-            Vector3 lookDir = hit.point - turret.transform.position;
-            lookDir.y = 0.0f;
-            lookDir = lookDir.normalized;
-            turretTargetRotation = Quaternion.LookRotation(lookDir);
+            Vector3 lookDir = hit.point - turret.transform.position;    // 탱크 위치에서 마우스가 가리키는 바닥지점으로 향하는 방향벡터
+            lookDir.y = 0.0f;               // y 제거
+            lookDir = lookDir.normalized;   // 노멀라이즈.
+            turretTargetRotation = Quaternion.LookRotation(lookDir);    // 최종적으로 포탑이 해야할 회전 계산
         }
     }
 
     private void OnNormalFire(InputAction.CallbackContext _)
     {
-        Instantiate(shellPrefab, firePosition.position, firePosition.rotation);
+        Instantiate(shellPrefab, firePosition.position, firePosition.rotation); // 일반 포탄 발사
     }
 
     void TurretTurn()
     {
+        // Update에서 계속 실행
+        // turretTargetRotation이 될때까지 회전
         turret.rotation = Quaternion.Slerp(turret.rotation, turretTargetRotation, turretTurnSpeed * Time.deltaTime);
     }
 
     private void Update()
     {
-        TurretTurn();
+        TurretTurn();   // 포탑만 돌리기
     }
 
     private void FixedUpdate()
     {
+        // 이동 처리        
         rigid.AddForce(inputDir.y * moveSpeed * transform.forward);
         rigid.AddTorque(inputDir.x * turnSpeed * transform.up);        
     }
