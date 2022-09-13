578bed807820b0d090453b0c9501af7ceddb5dc8
go2665 <77224762+go2665@users.noreply.github.com>
2022-09-13 16:46:15 +0900

    Tank - Shell_Badzone 스크립터블 오브젝트 작성 완료

--- 07_QuaterView/Assets/Scripts/Scriptable Object/ShellData_BadZone.cs
+++ 07_QuaterView/Assets/Scripts/Scriptable Object/ShellData_BadZone.cs
@@ -5,5 +5,28 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Shell Data(BadZone)", menuName = "Sctipable Object/Shell Data(BadZone)", order = 1)]
 public class ShellData_BadZone : ShellData
 {
+    public float effectDuration = 5.0f;
     public GameObject badEffectPrefab;
+
+    public override void Explosion(Vector3 position, Vector3 normal)
+    {
+        // 터지는 프리팹 생성
+        base.Explosion(position, normal);
+
+        // 안좋은 이팩트 생성
+        GameObject obj = Instantiate(badEffectPrefab, position, Quaternion.LookRotation(normal));
+
+        // 않좋은 이팩트 기간 설정
+        ParticleSystem[] pss = obj.GetComponentsInChildren<ParticleSystem>();
+        foreach (var ps in pss)
+        {
+            var main = ps.main;
+            main.duration = effectDuration;
+        }
+        pss[0].Play();
+
+        // 않좋은 이팩트가 주는 데미지 설정
+        BadEffect bedEffect = obj.GetComponent<BadEffect>();
+        bedEffect.damagePerSecond = damage / effectDuration;    // 전체 데미지 / 전체시간
+    }
 }
--- 07_QuaterView/Assets/Scripts/Shell_BadZone.cs
+++ 07_QuaterView/Assets/Scripts/Shell_BadZone.cs
@@ -4,35 +4,23 @@ using UnityEngine;
 
 public class Shell_BadZone : Shell
 {
-    public float effectDuration = 5.0f;
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        // 이팩트가 나올 바닥면 위치 구하기
+        Ray ray = new Ray(collision.contacts[0].point, Vector3.down);
+        Vector3 position;
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000.0f, LayerMask.GetMask("Ground")))
+        {
+            position = hit.point;
+        }
+        else
+        {
+            position = collision.contacts[0].point;
+        }
+        position += new Vector3(0, 0.1f, 0);
 
-    //protected override void OnCollisionEnter(Collision collision)
-    //{
-    //    Ray ray = new Ray(collision.contacts[0].point, Vector3.down);
-    //    Vector3 position;
-    //    Quaternion rotation = Quaternion.LookRotation(Vector3.up);
-    //    if( Physics.Raycast(ray, out RaycastHit hit, 1000.0f, LayerMask.GetMask("Ground")))
-    //    {
-    //        position = hit.point;
-    //    }
-    //    else
-    //    {
-    //        position = collision.contacts[0].point;
-    //    }
-    //    position += new Vector3(0, 0.1f, 0);
+        data.Explosion(position, Vector3.up);
 
-    //    GameObject obj = Instantiate(explosionPrefab, position, rotation);
-    //    ParticleSystem[] pss = obj.GetComponentsInChildren<ParticleSystem>();
-    //    foreach(var ps in pss)
-    //    {
-    //        var main = ps.main;
-    //        main.duration = effectDuration;            
-    //    }
-    //    pss[0].Play();
-
-    //    BadEffect bedEffect = obj.GetComponent<BadEffect>();
-    //    bedEffect.damagePerSecond = damage / effectDuration;    // 전체 데미지 / 전체시간
-
-    //    Destroy(this.gameObject);
-    //}
+        Destroy(this.gameObject);
+    }
 }
