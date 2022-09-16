de2868d4c6fff90d2e3a3d2c2d28267e10314dde
go2665 <77224762+go2665@users.noreply.github.com>
2022-09-16 18:13:25 +0900

    Tank - 주석 추가

--- 07_QuaterView/Assets/Scripts/EnemySpawner.cs
+++ 07_QuaterView/Assets/Scripts/EnemySpawner.cs
@@ -5,131 +5,148 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour, IHit
 {
-    public GameObject enemyPrefab;
+    public GameObject enemyPrefab;          // 스폰시킬 적 프리팹
 
     [Range(0,30)]
-    public float spawnInterval = 3.0f;
+    public float spawnInterval = 3.0f;      // 적 스폰 시간 간격
 
     [Range(1, 10)]
-    public int maxSpawnCount = 1;
+    public int maxSpawnCount = 1;           // 한번 활성화 했을 때 생성하는 적의 수
 
-    private float spawnTimer = 0.0f;
-    private int currentSpawnCount = 0;
+    private float spawnTimer = 0.0f;        // spawnInterval 시간 간격 확인용
+    private int currentSpawnCount = 0;      // 이번 활성화에서 생성한 적 수
 
-    bool waitMode = true;
+    private bool waitMode = true;           // 대기모드 여부(true면 대기 상태. false가 되면 적을 스폰하기 시작)
 
-    public float totalRepairTime = 5.0f;
-    float repairElapsed = 0.0f;
+    public float totalRepairTime = 5.0f;    // 스포너가 파괴되었을 때 정상으로 돌아오는데 걸리는 시간
+    private float repairElapsed = 0.0f;     // 파괴 이후 경과 시간
 
-    ParticleSystem fireEffect;
-    ParticleSystem explosionEffect;
+    private ParticleSystem fireEffect;      // 파괴 중 표시할 불 이팩트
+    private ParticleSystem explosionEffect; // 파괴 될 때 사용할 폭팔 이팩트
 
-    float hp;
-    float maxHP = 1000.0f;
-    bool isDead = false;
+    private float hp;                       // hp
+    private float maxHP = 1000.0f;          // 최대 hp
+    private bool isDead = false;            // 사망여부 (true면 사망)
     public float HP 
     { 
         get => hp;
         set 
         {
             hp = value;
-            //Debug.Log(hp);
             if (hp < 0)
             {
+                // HP가 0보다 작아지면 Dead함수 실행
                 hp = 0;
-                if (!isDead) // HP가 0보다 작아지면 Dead함수 실행
-                    Dead();
+                Dead();
             }
             hp = Mathf.Min(hp, maxHP);
+            onHealthChange?.Invoke();
         }
     }
 
     public float MaxHP { get => maxHP; }
 
-    public Action onHealthChange { get; set; }
+    public Action onHealthChange { get; set; }  
 
     public Action onDead { get; set; }
 
 
     void Awake()
     {
-        explosionEffect = transform.GetChild(3).GetComponent<ParticleSystem>();
+        explosionEffect = transform.GetChild(3).GetComponent<ParticleSystem>(); // 자식으로 있는 파티클 찾기
         fireEffect = transform.GetChild(4).GetComponent<ParticleSystem>();
     }
 
     private void Start()
     {
-        currentSpawnCount = 0;
+        // 시작할 때 값들 초기화
+        currentSpawnCount = 0;  
         hp = maxHP;
         isDead = false;
-        ResetSpawnTimer();
+        ResetSpawnTimer();  // 일정 시간 후에 스폰하도록 시간 초기화
     }
 
     private void Update()
     {
-        if (!waitMode && !isDead)
+        if (!waitMode && !isDead)   // wait 모드가 아니고 살아있으면
         {
-            spawnTimer -= Time.deltaTime;
-            if (spawnTimer < 0 && currentSpawnCount < maxSpawnCount)
+            spawnTimer -= Time.deltaTime;   // 적 생성을 위해 시간 감소
+            if (spawnTimer < 0 && currentSpawnCount < maxSpawnCount)    
             {
+                // 시간도 다 지나고 이번 활성화에서 생성할 수 있는 적 수가 남아있을 때 스폰
                 Spawn();
             }
         }
         if(isDead)
         {
-            repairElapsed += Time.deltaTime;
+            repairElapsed += Time.deltaTime;        // 스포너가 죽었으면 수리시간 증가
             if(repairElapsed > totalRepairTime)
             {
-                Restore();
+                Restore();                          // 충분히 다 수리되면 복구
             }
         }
     }
 
+    public void WaitModeOff()
+    {
+        // 스포너 컨트롤러의 델리게이트로 인해 실행되는 함수
+        // 스포너 컨트롤러가 활성화하라고 신호를 보낼 때 실행됨.
+        waitMode = false;       // 대기모드 해제하고
+        currentSpawnCount = 0;  // 스폰 회수 초기화
+    }
+
     private void Spawn()
     {
+        // 적 스폰하는 함수
         GameObject obj = Instantiate(enemyPrefab, 
             transform.position + UnityEngine.Random.insideUnitSphere * 0.1f, 
             transform.rotation);
-        //IHit hitTarget = obj.GetComponent<IHit>();
+
+        // 하나의 스포너가 계속 적을 생성할 때 사용하던 코드
+        //IHit hitTarget = obj.GetComponent<IHit>();        
         //hitTarget.onDead += ResetSpawnTimer;
         //hitTarget.onDead += () => currentSpawnCount--;
-        currentSpawnCount++;
-        ResetSpawnTimer();
+
+        currentSpawnCount++;    // 생성 갯수 증가
+        ResetSpawnTimer();      // 스폰 시간 초기화
 
         if( currentSpawnCount >= maxSpawnCount )
         {
-            waitMode = true;
+            waitMode = true;    // 최대치만큼 생성했으면 대기모드로 전환
         }
     }
 
     private void ResetSpawnTimer()
     {
-        spawnTimer = spawnInterval;
-    }    
-
-    public void WaitModeOff()
-    {
-        waitMode = false;
-        currentSpawnCount = 0;
+        // 한마리 스폰 후 시간 초기화하는 함수
+        spawnTimer = spawnInterval; // 스폰 시간 초기화
     }
 
     public void Dead()
     {
-        //Debug.Log("사망");
-        isDead = true;        
-        explosionEffect.Simulate(0);    // 파티클시스템 내부의 시간 재조정
-        explosionEffect.Play();
-        fireEffect.Simulate(0);
-        fireEffect.Play();
+        // 죽었을 때 실행될 함수
+        if (!isDead)
+        {
+            //Debug.Log("사망");
+            isDead = true;
+            explosionEffect.Simulate(0);    // 파티클 시스템 재사용을 위해 내부의 시간 재조정
+            explosionEffect.Play();         // 파티클 시스템 재생
+            fireEffect.Simulate(0);
+            fireEffect.Play();
+
+            onDead?.Invoke();       // 죽었다고 신호보내기
+        }
     }
 
     void Restore()
     {
+        // 스포너가 파괴되고 수리가 완료되었을 때 실행될 함수
+
         //Debug.Log("부활");
-        explosionEffect.Stop();
+        explosionEffect.Stop(); // 이팩트 끄고
         fireEffect.Stop();
-        isDead = false;
-        hp = maxHP;
-        repairElapsed = 0.0f;
+        isDead = false;         // 살았다고 표시
+        hp = maxHP;             // HP 최대로 올리고
+        repairElapsed = 0.0f;   // 수리 경과시간 초기화
     }
 }
--- 07_QuaterView/Assets/Scripts/EnemySpawnerController.cs
+++ 07_QuaterView/Assets/Scripts/EnemySpawnerController.cs
@@ -6,24 +6,26 @@ using UnityEngine.InputSystem;
 
 public class EnemySpawnerController : MonoBehaviour
 {
-    public float activateInterval = 5.0f;
+    public float activateInterval = 5.0f;   // 스포너 활성화 시간 간격
 
-    EnemySpawner[] enemySpawners;
-    Action[] onSpawnActivate;
+    EnemySpawner[] enemySpawners;           // 스포너 배열
+    Action[] onSpawnActivate;               // 스포너가 활성화 될 때 실행될 델리게이트 배열
 
-    int spawnerIndex = 0;
+    int spawnerIndex = 0;                   // 이번에 활성화 될 스포너의 인덱스
 
     private void Awake()
     {
-        enemySpawners = new EnemySpawner[transform.childCount];
-        onSpawnActivate = new Action[transform.childCount];
+        // 스포너 컨트롤러는 스포너만 자식으로 가진다.
+        enemySpawners = new EnemySpawner[transform.childCount]; // 자식 수만큼 배열 크기 설정
+        onSpawnActivate = new Action[transform.childCount];     // 스포너가 활성화 될 때 실행될 델리게이트가 같은 크기로 배열 설정
         for ( int i=0;i<transform.childCount;i++)
         {
-            enemySpawners[i] = transform.GetChild(i).GetComponent<EnemySpawner>();
-            onSpawnActivate[i] += enemySpawners[i].WaitModeOff;
+            enemySpawners[i] = transform.GetChild(i).GetComponent<EnemySpawner>();  // EnemySpawner 찾기
+            onSpawnActivate[i] += enemySpawners[i].WaitModeOff;                     // 활성화 시 실행될 함수 등록
         }
 
-        //enemySpawners = GetComponentsInChildren<EnemySpawner>();
+        // GetComponentsInChildren의 순서는 보장이 안되서 사용안함
+        //enemySpawners = GetComponentsInChildren<EnemySpawner>();  
         //onSpawnActivate = new Action[enemySpawners.Length];
         //for(int i=0; i<enemySpawners.Length; i++)
         //{
@@ -33,22 +35,22 @@ public class EnemySpawnerController : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(SpawnerActivator());
+        StartCoroutine(SpawnerActivator()); // 코루틴 실행
     }
 
     IEnumerator SpawnerActivator()
     {
         while (true)
         {
-            ActivateNextSpawner();
-            yield return new WaitForSeconds(activateInterval);
+            ActivateSpawner();          // 스포너 활성화
+            yield return new WaitForSeconds(activateInterval);  // activateInterval초 만큼 대기
         }
     }
 
-    void ActivateNextSpawner()
+    void ActivateSpawner()
     {
-        onSpawnActivate[spawnerIndex]?.Invoke();
-        spawnerIndex = (spawnerIndex + 1) % onSpawnActivate.Length;
+        onSpawnActivate[spawnerIndex]?.Invoke();                    // 델리게이트에 등록된 함수 실행
+        spawnerIndex = (spawnerIndex + 1) % onSpawnActivate.Length; // 인덱스 증가(최대치가 되면 다시 0으로)
     }
 
 }
