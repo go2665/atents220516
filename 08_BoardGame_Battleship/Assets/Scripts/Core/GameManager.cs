61eb7a3d1e4d14e7888fa9d32c44170c73135816
go2665 <77224762+go2665@users.noreply.github.com>
2022-09-30 15:45:03 +0900

    BattleShip - 배 배치 주석 추가 완료

--- 08_BoardGame_Battleship/Assets/Scripts/Core/GameManager.cs
+++ 08_BoardGame_Battleship/Assets/Scripts/Core/GameManager.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 싱글톤으로 구성된 게임 관리용 클래스
+/// </summary>
 public class GameManager : Singleton<GameManager>
 {
+    /// <summary>
+    /// 입력 처리용 컴포넌트
+    /// </summary>
     InputController input;
 
+    /// <summary>
+    /// 인풋 컨트롤러에 접근하기 위한 프로퍼티
+    /// </summary>
     public InputController Input { get => input; }    
 
     protected override void Awake()
     {
         base.Awake();
 
-        input = GetComponent<InputController>();
+        input = GetComponent<InputController>();    // 인풋 컨트롤러 찾기
     }
 
 }
--- 08_BoardGame_Battleship/Assets/Scripts/Core/ShipManager.cs
+++ 08_BoardGame_Battleship/Assets/Scripts/Core/ShipManager.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 배에 대한 전반적인 관리를 하는 클래스
+/// </summary>
 public class ShipManager : Singleton<ShipManager>
 {
     /// <summary>
@@ -25,18 +28,31 @@ public class ShipManager : Singleton<ShipManager>
     /// </summary>
     int shipDirectionCount = 0;
 
-    
+    /// <summary>
+    /// 배의 종류가 몇가지인지 알기 위한 프로퍼티
+    /// </summary>
     public int ShipTypeCount { get => shipTypeCount; }
 
+    /// <summary>
+    /// 배가 바라볼 수 있는 방향이 몇 방향인지 알기 위한 프로퍼티
+    /// </summary>
     public int ShipDirectionCount { get => shipDirectionCount; }
 
     protected override void Awake()
     {
         base.Awake();
+
+        // enum들의 값 가지수 찾아 저장하기
         shipTypeCount = Enum.GetValues(typeof(ShipType)).Length - 1;
         shipDirectionCount = Enum.GetValues(typeof(ShipDirection)).Length;
     }
 
+    /// <summary>
+    /// 배를 만드는 함수
+    /// </summary>
+    /// <param name="type">만들 배의 종류</param>
+    /// <param name="player">만들 배를 가지는 대상</param>
+    /// <returns>만든 배</returns>
     public Ship MakeShip(ShipType type, PlayerBase player)
     {
         GameObject shipObj = Instantiate(shipPrefab, player.transform);
@@ -48,6 +64,11 @@ public class ShipManager : Singleton<ShipManager>
         return ship;
     }
 
+    /// <summary>
+    /// 배 생성을 위한 배모델 가져오기
+    /// </summary>
+    /// <param name="type">리턴할 배의 종류</param>
+    /// <returns>배의 모델 프리팹</returns>
     public GameObject GetShipModel(ShipType type)
     {
         return shipModels[(int)type - 1];
