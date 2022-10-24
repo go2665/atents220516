3515d89a2fb77f3a2aba3525a2e4f3a3f7363192
go2665 <77224762+go2665@users.noreply.github.com>
2022-10-24 18:21:26 +0900

    BattleShip - 전투씬 UI 기본 세팅

--- 08_BoardGame_Battleship/Assets/Scripts/Player/EnemyPlayer.cs
+++ 08_BoardGame_Battleship/Assets/Scripts/Player/EnemyPlayer.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +5,9 @@ using UnityEngine.InputSystem;
 
 public class EnemyPlayer : PlayerBase
 {
+    public float thinkingTimeMin = 2.0f;
+    public float thinkingTimeMax = 5.0f;
+
     protected override void Start()
     {
         base.Start();
@@ -17,10 +19,19 @@ public class EnemyPlayer : PlayerBase
     public override void OnPlayerTurnStart()
     {
         base.OnPlayerTurnStart();   // 턴 시작 설정
+
+        // 적은 턴이 시작되면 몇초후에 자동으로 공격
+        StartCoroutine(TurnStartDelay());
     }
 
     public override void OnPlayerTurnEnd()
     {
+    }
+
+    IEnumerator TurnStartDelay()
+    {
+        yield return new WaitForSeconds(Random.Range(thinkingTimeMin, thinkingTimeMax));
+        AutoAttack();
     }
 
 }
