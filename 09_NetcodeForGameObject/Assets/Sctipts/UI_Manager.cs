b7b5793c93adf7982bf5ef061c2ce4e60b2dfff6
go2665 <77224762+go2665@users.noreply.github.com>
2022-10-31 16:19:43 +0900

    NetCode - Hello World
    NetCode For GameObject
    InputSystem
    TextMeshPro 추가

---
+++ 09_NetcodeForGameObject/Assets/Sctipts/UI_Manager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : Singleton<UI_Manager>
+{
+    public Button startHost;
+    public Button startClient;
+
+    private void Start()
+    {
+        startHost?.onClick.AddListener( () =>
+        {
+            if(NetworkManager.Singleton.StartHost())
+            {
+                Debug.Log("호스트가 시작되었습니다...");
+            }
+            else
+            {
+                Debug.Log("호스트 시작에 실패했습니다....");
+            }
+        });
+
+        startClient?.onClick.AddListener(() =>
+        {
+            if (NetworkManager.Singleton.StartClient())
+            {
+                Debug.Log("클라이언트가 연결을 시작하였습니다...");
+            }
+            else
+            {
+                Debug.Log("클라이언트가 연결에 실패했습니다....");
+            }
+        });
+    }
+}
